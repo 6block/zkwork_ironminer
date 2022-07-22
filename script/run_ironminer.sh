92e8fe8afd994e1bf900bfd22cacc7f60817f288
scu.wan <scu.wan@qtum.info>
2022-07-22 13:45:25 +0800

    Daemon configuration templates, including Systemd and Supervisor

---
+++ script/run_ironminer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export RUST_LOG=debug
+
+# change config
+pool=36.189.234.195:60006
+#pool=127.0.0.1:8181
+address="0df0b3222e32593d2f3103552326de56f5423ad4ba8064cbf4520ef2510a461426f10383d1c420ba73be3e"
+batch_size=10000
+cpus=`cat /proc/cpuinfo| grep "processor"| wc -l`
+echo "running zkwork_ironminer with pool($pool) address($address) batch($batch_size) threads{$cpus}"
+
+./zkwork_ironminer --pool $pool  --address $address --batch_size $batch_size --threads $cpus
