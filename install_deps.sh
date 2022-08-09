18b9bbfb53586e00849013b9a9b21cb6189fc556
hairtail <zhaoxinlong@web3.com>
2022-08-09 17:38:21 +0800

    FEAT: update to ironfish-v0.1.43

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [ ! -d "./ironfish" ];then
-    git clone -b v0.1.38 https://github.com/iron-fish/ironfish.git --depth 1
-    echo "deps installed(ironfish v0.1.38)."
+    git clone -b v0.1.43 https://github.com/iron-fish/ironfish.git --depth 1
+    echo "deps installed(ironfish v0.1.43)."
 fi
